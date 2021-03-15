cbdfcf87ad70fbde1ee3312303621588552236bd
stOne <silveredovoui@gmail.com>
2021-03-15 12:12:45 +0000

    Create BlogAdminBundleSleekDBBased.php

---
+++ BlogAdminBundleSleekDBBased.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * User: stOne
+ * Date: 15/03/2021
+ * Time: 12:11
+ */
+
+namespace DovStone\Bundle\BlogAdminBundleSleekDBBased;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+class BlogAdminBundleSleekDBBased extends Bundle
+{
+}
